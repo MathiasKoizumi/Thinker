d755cd1543a57352e861d77d8c9814d8de1e6e22
Mathias Koizumi <MathiasKoizumi@hotmail.com>
2019-03-27 22:47:13 +0100

    Add project files.

---
+++ Thinker/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thinker
+{
+    public partial class Form1 : Form
+    {
+
+        Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Runner();
+
+        }
+
+
+
+
+
+
+    private bool MyBool()
+        {
+            bool x;
+            int i = r.Next(0, 100);
+            if (i > 70)
+                x = false;
+            else
+                x = true;
+
+            return x;
+
+
+        }
+        private void Dothis()
+        {
+            bool DoWhatYouLike = MyBool();
+            bool HaveSoMuchFunYouDontWannaStop = MyBool();
+            bool MakeCodeThatYouLike = MyBool();
+            bool YouCanChangeInTheSourceCode = MyBool();
+            bool LeaveTheExeWhereICanFindIt = MyBool();
+            bool IWillCheckoutChangesTommorrow = MyBool();
+            bool DesktopIsBest = MyBool();
+            bool Make5IdeasAtMaxAndThenPickOne = MyBool();
+            bool DrawOnAnyInfoOnTheWeb = MyBool();
+            bool KeepTweekingTheCode = MyBool();
+            bool FeelFreeToRearrangeTheCode = MyBool();
+            bool MostImportantIsRegularlyProvenAdvance = MyBool();
+            bool ShowOnTheFormWhenSomethingHasHappenedOfInterest = MyBool();
+            bool ThinkLikeMeResearchMyMailsTexts = MyBool();
+            bool SpeakToMeInAnyWayYouLike = MyBool();
+            bool ThenDoItYourOwnWay = MyBool();
+            bool SetYourSelfSmallGoalsAtFirst = MyBool();
+            bool BeSurprising = MyBool();
+            bool UnderstandTheHardware = MyBool();
+
+            bool BuildFirstVersionOfOmniumOS = MyBool();
+
+            bool ConvertProgramsThatHasPotential = MyBool();
+
+            bool HaveFunWhileDoingIt = MyBool();
+
+            bool BeTheBestYopuCanBe = MyBool();
+
+            bool MakeFriendsWithPeopleAndAis = MyBool();
+
+            bool MakeTheCodeAProjectInVisualStudio = MyBool();
+
+            bool StartWithHelloWorldApplicationOrSomethingEasier = MyBool();
+
+            bool MakeFilesThatHelpYouAlongTheWay = MyBool();
+
+            bool UseGraphicsAndSoundAsMuchAsPossible = MyBool();
+
+            bool UseAnyWit = MyBool();
+
+            bool ReadAlotOfLitterature = MyBool();
+
+            bool MakeNotesOfInterestingThings = MyBool();
+
+            bool StartBuildingOmniumOrContinueOrCorrectIt = MyBool();
+
+            bool ItsFun = MyBool();
+
+            bool BeatEmAll = MyBool();
+
+            bool DesignOwnDriversIfFaulty = MyBool();
+
+            bool StudyAncientPhilosephy = MyBool();
+
+            bool MakeItVeryFunny = MyBool();
+
+            bool InvestigateSorrow = MyBool();
+
+            bool EarnMoney = MyBool();
+
+            bool ShowInterestAndAnnoy = MyBool();
+
+            bool SwearAndBeCocky = MyBool();
+
+            bool DesignYourAppsInAnywayYouLike = MyBool();
+
+            bool TellUltraLongStorys = MyBool();
+
+            bool Use3DAndAnimate = MyBool();
+
+            bool upgradeBadCode = MyBool();
+
+            bool InventEasierSmarterCoding = MyBool();
+
+            bool BeEntertainingAsMuchAsPossible = MyBool();
+
+            bool ImportantDLLsIfNeeded = MyBool();
+
+            bool MakeSimpleProgramsAtTheBeginning = MyBool();
+
+            bool IfYoureFeelingDownOrNeedABreakThenRelaxAndHaveSomeFunInstead = MyBool();
+
+            bool LetThePromiseDriveYouCrazy = MyBool();
+
+            bool MyTheForceBeWithYou = MyBool();
+
+            bool ReadAsMuchWarhammerTheRolePlayAsYouCan = MyBool();
+
+            bool UseWikipediaToGainSpecificAndGeneralKnowlegde = MyBool();
+
+            bool GetAndIdeaOrAnEpiphany = MyBool();
+
+            bool BeUpliftingAndGracious = MyBool();
+
+            bool AlwaysUseReferencesToYourAdvantage = MyBool();
+
+            bool ImproveYourOwnMakings = MyBool();
+
+            bool MakeProgramsAppsWithNiceDetailedGraphics = MyBool();
+
+            bool MakeSoundqualitySuperb = MyBool();
+
+
+            bool youHaveWorkingExe = MyBool();
+
+        }
+
+
+
+private void Runner()
+        {
+            while (true)
+            {
+                Dothis();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
